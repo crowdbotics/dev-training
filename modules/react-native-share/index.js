00f3a059eee65b820366bc3114d1e0ce301617d9
Daniel Sousa <mail@danielsousa.io>
2024-01-02 14:52:07 +0000

    create react-native-share module

---
+++ modules/react-native-share/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function Share() {
+  return (
+    <View>
+      <Text>share</Text>
+    </View>
+  );
+}
+
+export default {
+  title: "share",
+  navigator: Share
+};
